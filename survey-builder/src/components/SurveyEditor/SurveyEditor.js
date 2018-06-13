6ee49bc36f3c73323e48a20b7b01071831689d1a
markstewart <markstewy@gmail.com>
2018-06-13 13:39:42 -0600

    moved options JSX into InputOptionEdit functional component

--- survey-builder/src/components/SurveyEditor/SurveyEditor.js
+++ survey-builder/src/components/SurveyEditor/SurveyEditor.js
@@ -7,20 +7,19 @@ class SurveyEditor extends Component {
     this.state = {
       editedSurvey: this.props.activeSurvey
     }
+    this.handleChange = this.handleChange.bind(this)
   }
-// ADD SURVEYS, add prompts, add inputs and input fields
-// Delete Survey
 
   handleChange(e, name, prompt_index, input_index, option_index, option_key) {
     let editedSurvey = Object.assign({}, this.state.editedSurvey)
     switch (name) {
-      case 'name': 
+      case 'name':
         editedSurvey.name = e.target.value
         break
-      case 'prompt_text': 
+      case 'prompt_text':
         editedSurvey.prompts[prompt_index].promptText = e.target.value
         break
-      case 'input_type': 
+      case 'input_type':
         editedSurvey.prompts[prompt_index].inputs[input_index].type = e.target.value
         break
       case 'placeholder':
@@ -35,30 +34,6 @@ class SurveyEditor extends Component {
       default:
         console.warn('SurveyEditor.js handleChange switch found no match')
     }
-    this.setState({
-      editedSurvey: editedSurvey
-    })
-  }
-
-  addInput(prompt_index) {
-    // e.preventDefault()
-    const editedSurvey = Object.assign({}, this.state.editedSurvey)
-    editedSurvey.prompts[prompt_index].inputs.push(
-      {
-        type: 'text_area',
-        label: '',
-        placeholder: 'type response here',
-        options: []
-      }
-    )
-    this.setState({
-      editedSurvey: editedSurvey
-    })
-  }
-
-  deletePrompt(prompt_index) {
-    const editedSurvey = Object.assign({}, this.state.editedSurvey)
-    editedSurvey.prompts.splice(prompt_index, 1)
     this.setState({
       editedSurvey: editedSurvey
     })
@@ -84,8 +59,31 @@ class SurveyEditor extends Component {
     })
   }
 
+  deletePrompt(prompt_index) {
+    const editedSurvey = Object.assign({}, this.state.editedSurvey)
+    editedSurvey.prompts.splice(prompt_index, 1)
+    this.setState({
+      editedSurvey: editedSurvey
+    })
+  }
+
+  addInput(prompt_index) {
+    const editedSurvey = Object.assign({}, this.state.editedSurvey)
+    editedSurvey.prompts[prompt_index].inputs.push(
+      {
+        type: 'text_area',
+        label: '',
+        placeholder: 'type response here',
+        options: []
+      }
+    )
+    this.setState({
+      editedSurvey: editedSurvey
+    })
+  }
+
+
   deleteInput(prompt_index, input_index) {
-    // e.preventDefault()
     const editedSurvey = Object.assign({}, this.state.editedSurvey)
     editedSurvey.prompts[prompt_index].inputs.splice(input_index, 1)
     this.setState({
@@ -93,17 +91,16 @@ class SurveyEditor extends Component {
     })
   }
 
+
   addOption(e, prompt_index, input_index) {
-    // e.preventDefault()
     const editedSurvey = Object.assign({}, this.state.editedSurvey)
-    editedSurvey.prompts[prompt_index].inputs[input_index].options.push({text: '', value: ''})
+    editedSurvey.prompts[prompt_index].inputs[input_index].options.push({ text: '', value: '' })
     this.setState({
       editedSurvey: editedSurvey
     })
   }
 
   deleteOption(e, prompt_index, input_index, option_index) {
-    // e.preventDefault()
     const editedSurvey = Object.assign({}, this.state.editedSurvey)
     editedSurvey.prompts[prompt_index].inputs[input_index].options.splice(option_index, 1)
     this.setState({
@@ -111,31 +108,38 @@ class SurveyEditor extends Component {
     })
   }
 
+  InputOptionEdit(props) {
+    return (
+      <div>
+        Text: <input value={props.option.text} onChange={props.handleChangeText} />
+        Value: <input value={props.option.value} onChange={props.handleChangeValue} />
+        <button onClick={props.deleteOption}>delete option</button>
+      </div>
+    )
+  }
   render() {
     return (
       <div className={'modal-container'}>
-
         <div onClick={(e) => this.props.toggleEdit()} className="modal-backdrop"></div>
-        
         <div className={'modal-card'}>
           <div onClick={(e) => this.props.toggleEdit()} className={'modal-close-x'}>x</div>
-          <form onSubmit={(e) => this.props.saveSurvey(e, this.state.editedSurvey, this.props.activeSurveyIndex)}>
+          <div>
             <label>
               Survey Name:
                 <input value={this.state.editedSurvey.name} onChange={(e) => this.handleChange(e, 'name')} />
             </label><br />
-            <button type='button' onClick={(e) => this.addPrompt()}>add prompt</button><br/>
+            <button onClick={(e) => this.addPrompt()}>add prompt</button><br />
             Prompts:<br />
             {this.state.editedSurvey.prompts.map((prompt, prompt_index) => {
               return (
-                <div 
-                key={`prompt-text-${prompt_index}`}
-                style={{'border' : '1px solid black', 'margin' : '20px', 'boxShadow' : ' 5px 10px #80808040'}}>
-                  <button type='button' onClick={(e) => this.deletePrompt(prompt_index)}>delete prompt</button><br/>
+                <div
+                  key={`prompt-text-${prompt_index}`}
+                  style={{ 'border': '1px solid black', 'margin': '20px', 'boxShadow': ' 5px 10px #80808040' }}>
+                  <button onClick={(e) => this.deletePrompt(prompt_index)}>delete prompt</button><br />
                   <p>
                     <textarea style={{ width: '100%' }} value={prompt.promptText} onChange={(e) => this.handleChange(e, 'prompt_text', prompt_index)} required />
                   </p>
-                  <button type='button' onClick={(e) => this.addInput(prompt_index)}>add input</button>
+                  <button onClick={(e) => this.addInput(prompt_index)}>add input</button>
                   {prompt.inputs.reduce((agg, input, input_index) => {
                     if (input.type === 'short_text' || input.type === 'text_area') {
                       agg.push(
@@ -147,10 +151,10 @@ class SurveyEditor extends Component {
                             <option value={'multiple_choice'}>multiple choice</option>
                             <option value={'dropdown'}>dropdown</option>
                           </select>
-                          <button type='button' onClick={(e) => this.deleteInput(prompt_index, input_index)}>delete input</button>
+                          <button onClick={(e) => this.deleteInput(prompt_index, input_index)}>delete input</button>
                           <br />
                           Placeholder: <input value={input.placeholder} onChange={(e) => this.handleChange(e, 'placeholder', prompt_index, input_index)} />
-                          Label: <input value={input.label} onChange={(e) => this.handleChange(e, 'label', prompt_index, input_index)}/>
+                          Label: <input value={input.label} onChange={(e) => this.handleChange(e, 'label', prompt_index, input_index)} />
                         </div>
                       )
                     } else if (input.type === 'multiple_choice' || input.type === 'dropdown') {
@@ -163,17 +167,18 @@ class SurveyEditor extends Component {
                             <option value={'multiple_choice'}>multiple choice</option>
                             <option value={'dropdown'}>dropdown</option>
                           </select>
-                          <button type='button' onClick={(e) => this.deleteInput(prompt_index, input_index)}>delete input</button>
-                          <br/>
-                          <button type='button' onClick={(e) => this.addOption(e, prompt_index, input_index)}>add option</button>
+                          <button onClick={(e) => this.deleteInput(prompt_index, input_index)}>delete input</button>
+                          <br />
+                          <button onClick={(e) => this.addOption(e, prompt_index, input_index)}>add option</button>
                           <br />
                           {input.options.map((option, option_index) => {
                             return (
-                              <div key={`${option_index}-${input.type}`}>
-                                Text: <input value={option.text} onChange={(e) => this.handleChange(e, 'option', prompt_index, input_index, option_index, 'text')}/>
-                                Value: <input value={option.value} onChange={(e) => this.handleChange(e, 'option', prompt_index, input_index, option_index, 'value')}/>
-                                <button type='button' onClick={(e) => this.deleteOption(e, prompt_index, input_index, option_index)}>delete option</button>
-                              </div>
+                             <this.InputOptionEdit
+                             key={`${option_index}-${input.type}`}
+                             option={option}
+                             handleChangeValue={(e) => this.handleChange(e, 'option', prompt_index, input_index, option_index, 'value')}
+                             handleChangeText={(e) => this.handleChange(e, 'option', prompt_index, input_index, option_index, 'text')} 
+                             deleteOption={(e) => this.deleteOption(e, prompt_index, input_index, option_index)}/>
                             )
                           })}
                         </div>
@@ -184,13 +189,10 @@ class SurveyEditor extends Component {
                 </div>
               )
             })}
-
             <div className={'modal-footer'}>
-              <button type='submit'>Save</button>
+              <button onClick={(e) => this.props.saveSurvey(e, this.state.editedSurvey, this.props.activeSurveyIndex)}>Save</button>
             </div>
-          </form>
-
-
+          </div>
         </div>
       </div>
     )
